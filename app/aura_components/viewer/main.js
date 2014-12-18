0d6d864dae403b4f6afe1cba1203bf966d29ccf8
andreyr82 <andreyr82@gmail.com>
2014-12-18 16:08:01 +0300

    модальное окно

---
+++ app/aura_components/viewer/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Андрей on 18.12.14.
+ */
+define(['text!./viewer.hbs'], function(tpl) {
+    var template = _.template(tpl);
+    return {
+        type: 'Backbone',
+		  render: function(model) {
+			   this.html(template({'model': model}));
+				this.$find('.modal').modal();
+		  },
+        initialize: function() {
+            this.sandbox.on('post.show', this.render, this);
+        }
+    }
+});
