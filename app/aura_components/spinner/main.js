c4dd6b442507887f2a58c3f5657dfde55c1136bb
andreyr82 <andreyr82@gmail.com>
2014-12-19 11:20:31 +0300

    spinner

---
+++ app/aura_components/spinner/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Андрей on 19.12.14.
+ */
+define(['text!./spinner.hbs'], function(tpl) {
+    var template = _.template(tpl);
+    return {
+        type: 'Backbone',
+		  render: function() {
+				this.html(template());
+		  },
+		  toggle: function() {
+				this.$find('.spinner').toggle();
+		  }
+        initialize: function() {
+            this.sandbox.on('pipes.update', this.toggle, this);
+            this.sandbox.on('posts.updated', this.toggle, this);
+        }
+    }
+});
