d6e70708e0e16c5ea620dd05c534f301c860ecf9
andreyr82 <andreyr82@gmail.com>
2014-12-19 12:13:49 +0300

    загрузка компонентов

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ require(['bower_components/aura/lib/aura'], function (Aura) {
         .use('extensions/aura-backbone')
         .use('extensions/backbone-posts')
         .use('extensions/aura-pipes')
+		  .use('aura_components/spinner')
+		  .use('aura_components/posts')
+		  .use('aura_components/button')
         .start({ components: 'body' }).then(function () {
             console.warn('App started...');
         });
